a330fc5a64bc440ab021bd240cfa8471d7d08d28
scizorman <tueda1207@gmail.com>
2023-12-18 01:43:51 +0900

    Fix the warehouse resource

--- stacks/terraform/snowflake/warehouse.tf
+++ stacks/terraform/snowflake/warehouse.tf
@@ -18,10 +18,7 @@ resource "snowflake_warehouse" "dbt_xs" {
   auto_suspend                        = 60
   comment                             = "The XS warehouse used by dbt in dbt-playground."
   initially_suspended                 = true
-  max_cluster_count                   = 2
-  min_cluster_count                   = 1
   query_acceleration_max_scale_factor = 8
-  scaling_policy                      = "ECONOMY"
   statement_timeout_in_seconds        = 300
 
   lifecycle {
