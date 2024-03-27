d380973eeddb282055e4bb77844ff565c09325e2
scizorman <tueda1207@gmail.com>
2024-03-28 03:06:55 +0900

    Refactor the Makefile

--- stacks/terraform/local.mk
+++ stacks/terraform/local.mk
@@ -4,3 +4,5 @@ export SNOWFLAKE_ACCOUNT := tk83970.ap-northeast-1.aws
 export SNOWFLAKE_USER
 export SNOWFLAKE_PASSWORD
 export SNOWFLAKE_AUTHENTICATOR := Snowflake
+
+terraform_command := aws-vault exec dbt-playground -- terraform
